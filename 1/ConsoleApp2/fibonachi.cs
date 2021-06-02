dd38aee6364c077595e0cfa33149498a409c66cf
Dmitry <Dimon1998daf@mail.ru>
2021-06-02 09:09:03 +0600

    Lesson_1

---
+++ 1/ConsoleApp2/fibonachi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class fibonachi
+    {
+        
+        public int n
+        {
+            get;
+            set;
+        }
+        // без рекурсии
+        public int fibonachis(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            
+            for(int i=1; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+        // с рекурсией
+        public int fibonachirecurs(int n)
+        {
+            int FibonachiRecurs(int a, int b, int c)
+            {
+                return c == 0 ? a : FibonachiRecurs(b, a + b, c - 1);
+            }
+
+            int Fibonachi(int c)
+            {
+                return FibonachiRecurs(0, 1, c);
+            }
+            return Fibonachi(n);
+        }
+    }
+}
---
+++ 1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp2;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //к 1-ому заданию 1-я проверка
+        [TestMethod]
+        public void quest1_n10_return_не_простое_1()
+        {
+            //arange
+            int n = 25;
+            string answer = "не простое";
+
+            //act
+            quest1 q = new quest1();
+            string value = q.quests(n);
+
+            //assert
+            Assert.AreEqual(answer, value);
+           
+        }
+        
+        //к 1-ому заданию 2-я проверка
+        [TestMethod]
+        public void quest1_n10_return_не_простое_2()
+        {
+            //arange
+            int n = 30;
+            string answer = "не простое";
+
+            //act
+            quest1 q = new quest1();
+            string value = q.quests(n);
+
+            //assert
+            Assert.AreEqual(answer, value);
+           
+        }
+        
+        //к 1-ому заданию 3-я проверка
+        [TestMethod]
+        public void quest1_n10_return_не_простое_3()
+        {
+            //arange
+            int n = 50;
+            string answer = "не простое";
+
+            //act
+            quest1 q = new quest1();
+            string value = q.quests(n);
+
+            //assert
+            Assert.AreEqual(answer, value);
+           
+        }
+        
+        //к 1-ому заданию 4-я проверка
+        [TestMethod]
+        public void quest1_n10_return_простое_1()
+        {
+            //arange
+            int n = 31;
+            string answer = "простое";
+
+            //act
+            quest1 q = new quest1();
+            string value = q.quests(n);
+
+            //assert
+            Assert.AreEqual(answer, value);
+           
+        }
+        
+        //к 1-ому заданию 5-я проверка
+        [TestMethod]
+        public void quest1_n10_return_простое_2()
+        {
+            //arange
+            int n = 19;
+            string answer = "простое";
+
+            //act
+            quest1 q = new quest1();
+            string value = q.quests(n);
+
+            //assert
+            Assert.AreEqual(answer, value);
+           
+        }
+        
+        //к 1-ому заданию 6-я проверка
+        [TestMethod]
+        public void quest1_n10_return_простое_3()
+        {
+            //arange
+            int n = 23;
+            string answer = "простое";
+
+            //act
+            quest1 q = new quest1();
+            string value = q.quests(n);
+
+            //assert
+            Assert.AreEqual(answer, value);
+           
+        }
+
+        
+        // к 3-му заданию (без рекурсии)_1
+        [TestMethod]
+        public void fibonachi_int_10_answer_55()
+        {
+            //arange
+            int n = 10;
+            int answer = 55;
+
+            //act
+            fibonachi feb = new fibonachi();
+            int ans = feb.fibonachis(n);
+
+            //assert
+            Assert.AreEqual(answer, ans);
+
+        }
+        
+        // к 3-му заданию (без рекурсии)_2
+        [TestMethod]
+        public void fibonachi_int_17_answer_1597()
+        {
+            //arange
+            int n = 17;
+            int answer = 1597;
+
+            //act
+            fibonachi feb = new fibonachi();
+            int ans = feb.fibonachis(n);
+
+            //assert
+            Assert.AreEqual(answer, ans);
+
+        }
+        
+        // к 3-му заданию (без рекурсии)_3
+        [TestMethod]
+        public void fibonachi_int_36_answer_14930352()
+        {
+            //arange
+            int n = 36;
+            int answer = 14930352;
+
+            //act
+            fibonachi feb = new fibonachi();
+            int ans = feb.fibonachis(n);
+
+            //assert
+            Assert.AreEqual(answer, ans);
+
+        }
+        
+        // к 3-му заданию с рекурсией_4
+        [TestMethod]
+        public void fibonachirecurs_int_15_answer_610()
+        {
+            //arange
+            int n = 15;
+            int answer = 610;
+
+            //act
+            fibonachi feb = new fibonachi();
+            int ans = feb.fibonachirecurs(n);
+
+            //assert
+            Assert.AreEqual(answer, ans);
+
+        }
+        
+        // к 3-му заданию с рекурсией_5
+        [TestMethod]
+        public void fibonachirecurs_int_20_answer_6765()
+        {
+            //arange
+            int n = 20;
+            int answer = 6765;
+
+            //act
+            fibonachi feb = new fibonachi();
+            int ans = feb.fibonachirecurs(n);
+
+            //assert
+            Assert.AreEqual(answer, ans);
+
+        }
+        
+        // к 3-му заданию с рекурсией_6
+        [TestMethod]
+        public void fibonachirecurs_int_10_answer_55()
+        {
+            //arange
+            int n = 10;
+            int answer = 55;
+
+            //act
+            fibonachi feb = new fibonachi();
+            int ans = feb.fibonachirecurs(n);
+
+            //assert
+            Assert.AreEqual(answer, ans);
+
+        }
+        
+    }
+}
